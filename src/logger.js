f03c101ba8df38c01cca4c4846d3f17e136fbadd
bhavishy khandelwal <khandelwalbhavishy@gmail.com>
2025-11-08 19:19:03 +0530

    add color-coded log levels using chalk

--- src/logger.js
+++ src/logger.js
@@ -3,3 +3,9 @@ module.exports = {
   info: (...args) => console.log(`[INFO ${timestamp()}]`, ...args),
   error: (...args) => console.error(`[ERR ${timestamp()}]`, ...args)
 };
+const chalk = require('chalk');
+module.exports = {
+  info: (...args) => console.log(chalk.blue(`[INFO ${new Date().toISOString()}]`), ...args),
+  warn: (...args) => console.warn(chalk.yellow(`[WARN ${new Date().toISOString()}]`), ...args),
+  error: (...args) => console.error(chalk.red(`[ERROR ${new Date().toISOString()}]`), ...args)
+};
